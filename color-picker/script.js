79c67304af0e09b553b1482f3e069c244e3aef21
DmIrina <73281267+DmIrina@users.noreply.github.com>
2022-04-01 05:40:36 +0300

    3

---
+++ color-picker/script.js
@@ -0,0 +1,16 @@
+
+function doColorOnChange() {
+  var dd1 = document.getElementById("d1");
+  var colorinput =
+document.getElementById("clr1");
+  var color = colorinput.value;
+  dd1.style.backgroundColor = color;
+}
+
+function doColorOnCLick() {
+  var dd1 = document.getElementById("d1");
+  var colorinput =
+document.getElementById("clr2");
+  var color = colorinput.value;
+  dd1.style.backgroundColor = color;
+}
